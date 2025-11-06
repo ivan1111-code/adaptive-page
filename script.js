e40d65179c4db02f37df13f1db31ef3b36e4cacf
ivan1111-code <ivanglush1004@yandex.ru>
2025-11-06 15:06:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+//тексты
+let upper_text = "We provide a wide range of creative services";
+
+let pictire1_upper_text = "Hight Quality"
+let pictire2_upper_text = "Creative Team"
+let pictire3_upper_text = "Amazing Results"
+let pictire4_upper_text = "Best Pricing"
+let pictire5_upper_text = "Innovations"
+let pictire6_upper_text = "24/7 Support"
+
+let pictire_text = "Vitae suscipit tellus mauris a diam maecenas sed enim ut. Mauris augue neque gravida in fermentum. Praesent semper feugiat nibh sed pulvinar proin id venenatis."
+
+//upper
+const upper = document.querySelector('.upper_p');
+upper.textContent = upper_text;
+//picture upper text
+const picture1 = document.querySelector('.text1')
+picture1.textContent = pictire1_upper_text;
+const picture2 = document.querySelector('.text2')
+picture2.textContent = pictire2_upper_text;
+const picture3 = document.querySelector('.text3')
+picture3.textContent = pictire3_upper_text;
+const picture4 = document.querySelector('.text4')
+picture4.textContent = pictire4_upper_text;
+const picture5 = document.querySelector('.text5')
+picture5.textContent = pictire5_upper_text;
+const picture6 = document.querySelector('.text6')
+picture6.textContent = pictire6_upper_text;
+//picture text
+const desc1 = document.querySelector 
+('.text_description1') 
+desc1.textContent = pictire_text;
+
+const desc2 = document.querySelector 
+('.text_description2') 
+desc2.textContent = pictire_text;
+
+const desc3 = document.querySelector 
+('.text_description3') 
+desc3.textContent = pictire_text;
+
+const desc4 = document.querySelector 
+('.text_description4') 
+desc4.textContent = pictire_text;
+
+const desc5 = document.querySelector 
+('.text_description5') 
+desc5.textContent = pictire_text;
+
+const desc6 = document.querySelector 
+('.text_description6') 
+desc6.textContent = pictire_text;
+
